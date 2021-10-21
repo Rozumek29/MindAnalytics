b766bfe6e3adeb33dc6cce8ad5efdd8f0d996959
BuildTools <unconfigured@null.spigotmc.org>
2021-10-21 13:29:49 +0200

    Initial commit

---
+++ src/main/java/com/github/rozumek29/mindanalytics/MindAnalytics.java
@@ -0,0 +1,17 @@
+package com.github.rozumek29.mindanalytics;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MindAnalytics extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
